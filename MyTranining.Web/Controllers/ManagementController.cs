f421b0341b12e913c90380df2f0fc4df1b053bf2
zhangzhaohui <zhangzhaohui@yiguo.com>
2018-05-25 17:34:17 +0800

    添加Core MVC web 版本

---
+++ MyTranining.Web/Controllers/ManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTranining.Web.Controllers
+{
+    public class ManagementController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
